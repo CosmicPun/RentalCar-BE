99499b847dd85dad5dc4e6f200fc056ea9b29e39
kogorosleepy <bennyaphaa4869@gmail.com>
2026-04-13 14:10:53 +0700

    add Review Schema

---
+++ models/Review.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        providerId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Provider",
+            required: true,
+        },
+        bookingId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Booking",
+            required: true,
+            unique: true,
+        },
+        rating: {
+            type: Number,
+            required: true,
+            min: 1,
+            max: 5,
+        },
+       comment: {
+            type: String,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model("Review", reviewSchema);
